f1da0e3a1e5880c5acd60cf8258e12d3ab404420
AkashHanumanthappa <akashhanumanthappa.official@gmail.com>
2025-11-18 12:44:43 +0530

    url set

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:4200'], // allow both frontend ports
+  origin: ['http://localhost:3000', 'http://localhost:4201'], // allow both frontend ports
   credentials: true
 }));
 
